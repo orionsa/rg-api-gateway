47b88532018e315c29afeb75c222dcf551508a1d
Orion Sasson <orionsasson@gmail.com>
2024-01-22 21:38:08 +0200

    feat(main): add proxy to user service

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { MicroserviceOptions } from '@nestjs/microservices';
+import { createProxyMiddleware } from 'http-proxy-middleware';
 
 import { AppModule } from './app.module';
 import { kafkaConfig } from './utils/kafkaConfig';
@@ -8,6 +9,12 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.connectMicroservice<MicroserviceOptions>(kafkaConfig);
   app.startAllMicroservices();
+  app.use(
+    '/api/v1/user',
+    createProxyMiddleware({
+      target: 'http://localhost:3001',
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
